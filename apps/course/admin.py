15654e419a10548573c4f2e6fdbad132ae7080f1
jakhongirs <jakhongirsv@gmail.com>
2025-09-06 14:58:35 +0500

    feat: add Course model management to admin interface with display and filter options

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.course.models import Subject
+from apps.course.models import Course, Subject
 
 
 @admin.register(Subject)
@@ -8,3 +8,10 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "is_active")
     search_fields = ("title", "slug")
     list_filter = ("is_active",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "is_active")
+    search_fields = ("title", "slug")
+    list_filter = ("is_active", "is_unlimited", "is_main_course", "subject")
