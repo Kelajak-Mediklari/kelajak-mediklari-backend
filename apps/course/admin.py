40803e8606b4f01497531046f23dc6b060749bfc
jakhongirs <jakhongirsv@gmail.com>
2025-09-13 16:48:55 +0500

    feat: enhance admin interface by adding management for Gallery, File, and LessonPart models with display and filter options

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 
-from apps.course.models import Course, Lesson, Roadmap, Subject
+from apps.course.models import (
+    Course,
+    File,
+    Gallery,
+    Lesson,
+    LessonPart,
+    Roadmap,
+    Subject,
+)
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "image",
+    )
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "file",
+    )
 
 
 @admin.register(Roadmap)
@@ -28,3 +52,10 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "is_active")
     search_fields = ("title", "slug")
     list_filter = ("is_active", "course")
+
+
+@admin.register(LessonPart)
+class LessonPartAdmin(admin.ModelAdmin):
+    list_display = ("title", "lesson", "type", "is_active")
+    search_fields = ("title",)
+    list_filter = ("is_active", "lesson", "type")
