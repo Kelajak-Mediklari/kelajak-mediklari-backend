eca345fe63a5912b34dd4ab9838710dd653f0d74
muhammad <zabborovmuhammad291@gmail.com>
2025-09-20 16:46:53 +0500

    fix

--- apps/users/api_endpoints/profile/ProfileUpdate/views.py
+++ apps/users/api_endpoints/profile/ProfileUpdate/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .serializers import ProfileUpdateSerializer
 
@@ -7,6 +8,8 @@ from .serializers import ProfileUpdateSerializer
 class ProfileUpdateView(UpdateAPIView):
     serializer_class = ProfileUpdateSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
 
     def get_object(self):
         return self.request.user
--- apps/users/models.py
+++ apps/users/models.py
@@ -14,6 +14,7 @@ class User(AbstractUser, BaseModel):
     class Gender(models.TextChoices):
         MALE = "MALE", _("Male")
         FEMALE = "FEMALE", _("Female")
+    
 
     full_name = models.CharField(
         verbose_name=_("Full name"), max_length=255, null=True, blank=True
