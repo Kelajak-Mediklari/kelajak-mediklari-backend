acb93807041bfd540a2ad8ddcc36850c7c47fe50
Jakhongir Sagdullaev <jakhongirs@Jakhongirs-MacBook-Air.local>
2025-11-12 22:02:17 +0500

    feat: add is_free_trial field to UserCoursesListSerializer

--- apps/course/api_endpoints/course/UserCoursesList/serializers.py
+++ apps/course/api_endpoints/course/UserCoursesList/serializers.py
@@ -22,6 +22,7 @@ class UserCoursesListSerializer(serializers.ModelSerializer):
             "start_date",
             "finish_date",
             "is_expired",
+            "is_free_trial",
             "completed_lessons_count",
             "is_group_member",
         )
@@ -29,7 +30,9 @@ class UserCoursesListSerializer(serializers.ModelSerializer):
     def get_completed_lessons_count(self, obj):
         """Get the count of completed lessons for this user course"""
         return obj.user_lessons.filter(is_completed=True).count()
-    
+
     def get_is_group_member(self, obj):
         """Check if the user is a group member"""
-        return GroupMember.objects.filter(user=self.context['request'].user, group__course=obj.course, is_active=True).exists()
+        return GroupMember.objects.filter(
+            user=self.context["request"].user, group__course=obj.course, is_active=True
+        ).exists()
